d961a19a3e3ef001ec1c4977e500c5b7fd5f845b
HDA1823 <139831720+HDA1823@users.noreply.github.com>
2026-09-25 09:55:32 -0300

    Add files via upload

---
+++ app/src/main/java/com/nataliabraga/caminhodocarro/MainActivity.java
@@ -0,0 +1,35 @@
+package com.nataliabraga.caminhodocarro;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setAllowFileAccess(true);
+        settings.setAllowContentAccess(true);
+        settings.setBuiltInZoomControls(false);
+        settings.setDisplayZoomControls(false);
+        settings.setSupportZoom(false);
+        settings.setTextZoom(100);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+
+    @Override
+    public void onBackPressed() {
+        WebView webView = (WebView) findViewById(android.R.id.content);
+        super.onBackPressed();
+    }
+}
